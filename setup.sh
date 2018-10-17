c6f3f5e377e3794b9ce83a7443f73b7b7709b48c
zootos <halvard.hummel@hotmail.com>
2018-10-17 18:26:18 +0200

    Fixed a couple of mistakes

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ echo "KEY = '$secret'" >> local_settings.py
 
 
 # Virtual environment
+sudo apt install virtualenv
 virtualenv -p python3 env_stream
 source env_stream/bin_activate
 pip install -r requirements.txt
