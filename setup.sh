c1a445cd8ba3918c0096761fe3f4b5ac460c4ef1
Dabble <ddabble@users.noreply.github.com>
2022-07-14 13:04:31 +0200

    Use venv instead of virtualenv

--- setup.sh
+++ setup.sh
@@ -16,15 +16,20 @@ echo "KEY = '$secret'" >> local_settings.py
 
 
 # Install dependencies
-sudo apt install libopenjp2-7 libtiff5 libjpeg-dev python3-dev
+sudo apt install libopenjp2-7 libtiff5 libjpeg-dev python3-dev python3-venv
 
 
 # Create virtual environment
-sudo apt install virtualenv
-virtualenv -p python3 env_stream
-source env_stream/bin/activate
+python3 -m venv venv
+source venv/bin/activate
+python -m pip install --upgrade pip
 pip install -r requirements.txt
+# The `deactivate` command of the `venv` package has an unset variable bug.
+# This is fixed in a newer version of Python than 3.7 (which is the version that currently comes with OctoPi),
+# but let's delay the hassle of upgrading until later :))
+set +u
 deactivate
+set -u
 
 
 # Set up and start service
